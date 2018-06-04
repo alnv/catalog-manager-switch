74e4cc8e005a6e9b70de35e5faceef67e456b0ee
Alexander Naumov <me@alexandernaumov.de>
2018-06-04 12:57:32 +0200

    add labels

---
+++ languages/de/tl_switch_template_controller.php
@@ -0,0 +1,29 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['new'] = [ 'Switch anlegen', 'Neuen Switch anlegen.' ];
+
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['view_settings'] = 'Ansicht-Einstellungen';
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['template_settings'] = 'Template-Einstellungen';
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['general_settings'] = 'Allgemeine-Einstellungen';
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['pagination_legend'] = 'Seitennummerierung-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['edit'] = [ 'Switch bearbeiten', 'Hier können Sie den Switch Controller bearbeiten.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['delete'] = [ 'Switch löschen', 'Hier können Sie den Switch Controller löschen.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['show'] = [ 'Switch Anzeigen', 'Hier können Sie den Switch Controller anzeigen.' ];
+
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['name'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung ein.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['listTemplate'] = [ 'Listen-Template', 'Bitte wählen Sie ein Template aus.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['customTemplate'] = [ 'Haupt-Template', 'Bitte wählen Sie ein Template aus.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['icon'] = [ 'Bild', 'Bitte wählen Sie ein Bild für den Navigationspunkt aus.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['moduleId'] = [ 'Frontend Modul', 'Bitte wählen Sie das passende Frontend Modul aus.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['overwriteFastMode'] = [ 'Fast Mode überschreiben', 'Hier können Sie den FastMode überschreiben.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['enableTableView'] = [ 'Als Tabelle darstellen', 'Alle Datensätze werden in einer Tabelle dargestellt.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['activeTableColumns'] = [ 'Kopfzeilen auswählen', 'Hier können Sie Kopfzeilen für die Tabelle auswählen.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['overwriteTableView'] = [ 'Tabellenansicht überschreiben', 'Hier können Sie die Tabellenansicht überschreiben.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['preventFieldFromFastMode'] = [ 'Felder ausschließen', 'Hier können Sie die Felder vom "Fast Mode" ausschließen.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['fastMode'] = [ 'Fast Mode', 'Hier können Sie den Fast Mode aktivieren. Folgende Elemente werden nicht gerendert: Datei-Upload und Kartenansicht.' ];
+
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['addPagination'] = [ 'Seitennummerierung hinzufügen', 'Hier können Sie die Seitennummerierung hinzufügen.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['offset'] = [ 'Datensätze überspringen', 'Hier legen Sie fest, wie viele Datensätze übersprungen werden sollen.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['overwritePagination'] = [ 'Seitennummerierung überschreiben', 'Hier können Sie die Seitennummerierung überschreiben.' ];
+$GLOBALS['TL_LANG']['tl_switch_template_controller']['perPage'] = [ 'Datensätze pro Seite', 'Die Anzahl an Datensätzen pro Seite. Geben Sie 0 ein, um den automatischen Seitenumbruch zu deaktivieren.' ];
