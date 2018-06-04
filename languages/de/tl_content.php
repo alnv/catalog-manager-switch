e4a010d3bdb49dd0a7af02bf5a774036afa069ad
Alexander Naumov <me@alexandernaumov.de>
2018-06-04 12:57:43 +0200

    add labels

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -1,2 +1,4 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_content']['switch_template_legend'] = 'Switch-Einstellungen';
+$GLOBALS['TL_LANG']['tl_content']['switchTemplateController'] = [ 'Switch Controller', 'Hier sehen Sie Ihre Switch-Controller.' ];
